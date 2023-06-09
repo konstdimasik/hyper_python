fc332bf8ecb2ea74237e81cd02f688078ec09142
Dmitry Konstantinov <konstdimasik@gmail.com>
2023-06-09 20:13:31 +0300

    refactor

--- duskers/duskers.py
+++ duskers/duskers.py
@@ -1,28 +1,40 @@
-from welcome_art import welcome_screen
+from ascii_art import welcome_screen, game_hub, robot
 
 
 class SurvivalGame():
     def __init__(self):
-        self.welcome_screen = welcome_screen
-        self.main_commands = {
+        self._welcome_screen = welcome_screen
+        self._game_not_over = True
+        self._main_commands = {
             'play': self.ask_for_play,
             'exit': self.exit_game,
+            'high': self.high_scores,
+            'help': self.help,
+            'back': self.run,
         }
-        self.play_commands = {
+        self._play_commands = {
             'yes': self.play,
             'no': self.dont_play,
+            'menu': self.run
         }
+
+    def finish_game(self) -> None:
+        self._game_not_over = False
 
     def exit_game(self) -> None:
         print('Thanks for playing, bye!')
-        exit()
+        self.finish_game()
+
+    def help(self) -> None:
+        print('Coming SOON! Thanks for playing!')
+        self.finish_game()
 
     def ask_for_play(self) -> None:
         name = input('Enter your name:\n')
         print(f'Greetings, commander {name}!')
         while True:
             print('Are you ready to begin?')
-            print('    [Yes] [No]')
+            print('[Yes] [No] Return to Main[Menu]')
             ready = input('Your command:\n').lower()
             try:
                 self.play_commands.get(ready)()
@@ -30,16 +42,21 @@ class SurvivalGame():
                 print('Invalid input')
 
     def play(self) -> None:
-        print("Great, now let's go code some more ;)")
-        exit()
+        print(game_hub)
+        self.finish_game()
 
     def dont_play(self) -> None:
         print('How about now.')
 
+    def high_scores(self) -> None:
+        print('No scores to display.')
+        print('[Back]')
+        self.run()
+
     def run(self) -> None:
         print(welcome_screen)
-        print('\t[Play]\n\t[Exit]\n')
-        while True:
+        print('[Play]\n[High] scores\n[Help]\n[Exit]\n')
+        while self.game_not_over:
             entry = input('Your command:\n').lower()
             try:
                 self.main_commands.get(entry)()
